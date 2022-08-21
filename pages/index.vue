4b1581c5de7763fe7428ee3b2f60d1c29a49fb80
GitHubrx <2097574648@qq.com>
2022-08-21 18:43:40 +0800

    第一次

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    你好Nuxt
+<!--
+  echo "# NuxtDemo1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GitHubrx/NuxtDemo1.git
+git push -u origin main
+  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
